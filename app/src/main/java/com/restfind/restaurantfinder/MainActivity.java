cadf79f43948857030179f70bebefb1bddce60d8
Melkizedek <martin_reif@yahoo.com>
2015-11-27 11:44:05 +0100

    asdf

--- app/src/main/java/com/restfind/restaurantfinder/MainActivity.java
+++ app/src/main/java/com/restfind/restaurantfinder/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // Test sdfs
-    private String name = "test";
+    private String name = "asdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
