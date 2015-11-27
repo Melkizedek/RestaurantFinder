07d1987acec72e40084614728af5264be24cc331
Melkizedek <martin_reif@yahoo.com>
2015-11-27 12:59:30 +0100

    API Key

--- app/src/main/java/com/restfind/restaurantfinder/MainActivity.java
+++ app/src/main/java/com/restfind/restaurantfinder/MainActivity.java
@@ -5,14 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Test sdfs
-    private String na,sme = "asdf";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //testjeislaöjtelöaj
+
     }
 }
