e5ae73bbe4046048f7fd87bf9d251de631f58ef9
Melkizedek <martin_reif@yahoo.com>
2015-11-27 11:35:10 +0100

    Initial commit

---
+++ app/src/main/java/com/restfind/restaurantfinder/MainActivity.java
@@ -0,0 +1,15 @@
+package com.restfind.restaurantfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String name = "test";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
