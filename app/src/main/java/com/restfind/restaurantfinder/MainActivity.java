9c4e437d9b8f92c5f58f66661de883e277bd550b
Schoergi <Gabriel.Schoerghuber@gmx.at>
2015-11-27 11:43:20 +0100

    Test123

--- app/src/main/java/com/restfind/restaurantfinder/MainActivity.java
+++ app/src/main/java/com/restfind/restaurantfinder/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // Test sdfs
-    private String name = "test";
+    private String name = "test23";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
